fixed timing units and added dynamical color scale as option to config. most up to date is pds_ml

Loader.py
@@ -143,17 +143,22 @@ class Loader:
             part_keys = [key for key in tree.keys() if 'mcpart_' in key]
             parts = tree.arrays(self.hdrkeys+part_keys,library='pd')
 
-            shower_keys = [key for key in tree.keys() if 'mcshower_' in key]
-            showers = tree.arrays(shower_keys,library='pd')
-
-            track_keys = [key for key in tree.keys() if 'mctrack_' in key]
-            tracks = tree.arrays(track_keys,library='pd')
+            #Correct columns with strings
+            processes = tree.arrays(['mcpart_process'],library='np')
+            endprocesses = tree.arrays(['mcpart_endprocess'],library='np')
             
-            #Valid particles filter
-            valid_tracks = tracks.mctrack_TrackId.values
-            valid_showers = showers.mcshower_TrackId.values
-            valid_parts = np.append(valid_showers,valid_tracks)
-            self.mcpart_df = parts[parts['mcpart_TrackId'].isin(valid_parts)]
+            mcpart_processes = []
+            mcpart_endprocesses = []
+            for p in processes['mcpart_process']:
+                mcpart_processes += p
+            for p in endprocesses['mcpart_endprocess']:
+                mcpart_endprocesses += p
+
+            parts['mcpart_process'] = mcpart_processes
+            parts['mcpart_endprocess'] = mcpart_endprocesses
+            
+            #Filter by primary
+            self.mcpart_df = parts.query('mcpart_process == "primary"')
             
         else:
             self.mcpart_df = None
@@ -226,7 +231,7 @@ class Loader:
             s2 = time()
             if self.mode == 'op':
                 op_df = self.op_evt[self.op_evt.ophit_opch == i]
-                op_times = op_df.ophit_peakT.values
+                op_times = op_df.ophit_peakT.values*1e3 #Convert to ns
                 op_pes = op_df.ophit_pe.values
             elif self.mode == 'prompt' or self.mode == 'prelim':
                 op_df = self.op_evt[self.op_evt.ch_ID == i]
@@ -270,7 +275,7 @@ class Loader:
         
         mcparts = []
         #Extract info into arrays
-        pdgs, engs, pxs, pys, pzs, x1s, y1s, z1s, x2s, y2s, z2s = (
+        pdgs, engs, pxs, pys, pzs, x1s, y1s, z1s, x2s, y2s, z2s, processes, endprocesses = (
             self.mcpart_evt.mcpart_pdg.values,
             self.mcpart_evt.mcpart_Eng.values,
             self.mcpart_evt.mcpart_Px.values,
@@ -282,13 +287,18 @@ class Loader:
             self.mcpart_evt.mcpart_EndPointx.values,
             self.mcpart_evt.mcpart_EndPointy.values,
             self.mcpart_evt.mcpart_EndPointz.values,
+            self.mcpart_evt.mcpart_process.values,
+            self.mcpart_evt.mcpart_endprocess.values,
         )
         if keep_pdgs is None:
             for i in range(len(pdgs)):
-                mcparts.append(MCPart(pdgs[i], engs[i], pxs[i], pys[i], pzs[i], x1s[i], y1s[i], z1s[i], x2s[i], y2s[i], z2s[i]))
+                mcparts.append(MCPart(pdgs[i], engs[i], pxs[i], pys[i], pzs[i], x1s[i], y1s[i], z1s[i], x2s[i], y2s[i], z2s[i], processes[i], endprocesses[i]))
         return mcparts
     def get_crt_list(self):
         pass
+    def get_pe_centroid(self,coating=0):
+        pass
+        
             
             
 

PMT.py
@@ -23,7 +23,33 @@ class PMT:
             self.op_pe = None
             self.bins = None
 
-    def plot_coordinates(self, start, end, cmap='magma', cmin=0, cmax=2000):
+    def get_pe_start_stop(self,start,end):
+        """
+        Get pe within start and stop times for bins
+
+        Args:
+            op_pe (pandas dataframe): contains two columns, 'time_bin' which are pd intervals, and op_pe which contain the pe for that time bin.
+            bins (numpy array): time bins aranged in even intervals
+            start (number): start time
+            end (number): end time
+
+        Returns:
+            _type_: _description_
+        """
+        start_ind = np.searchsorted(self.bins,start) 
+        end_ind = np.searchsorted(self.bins,end)
+        if start_ind < end_ind:
+            mask = np.full(len(self.op_pe),False)
+            inds = list(range(start_ind,end_ind))
+            mask[inds] = True
+            color = self.op_pe.loc[mask, 'op_pe'].sum()
+        else:
+            color = 0
+        if np.isnan(color):
+            color = 0
+        return color
+
+    def plot_coordinates(self, start, end, cmap='magma', cmin=0, cmax=None):
         """ 
         Plots PMTs onto PAD grid
 
@@ -32,7 +58,7 @@ class PMT:
             end (_type_): end time window
             cmap (str, optional): Defaults to 'magma'.
             cmin (int, optional): Defaults to 0.
-            cmax (int, optional): Defaults to 10000.
+            cmax (int, optional): Defaults to None.
 
         Returns:
             go.Scatter: scatter point to be plotted on dash canvas
@@ -44,21 +70,7 @@ class PMT:
             showscale = True
         else:
             showscale = False
-        if self.op_pe is not None:
-            s0 = time()
-            start_ind = np.searchsorted(self.bins,start) 
-            end_ind = np.searchsorted(self.bins,end)
-            if start_ind < end_ind:
-                mask = np.full(len(self.op_pe),False)
-                inds = list(range(start_ind,end_ind))
-                mask[inds] = True
-                color = self.op_pe.loc[mask, 'op_pe'].sum()
-            else:
-                color = 0
-            if np.isnan(color):
-                color = 0
-        else:
-            color = 0
+        color = self.get_pe_start_stop(start, end)
         s1 = time()
         #if VERBOSE: print(f'-- time to bin {s1-s0:.3f} s')
         msize = 18

run_pad.py
@@ -64,7 +64,13 @@ l.get_event(run,subrun,event)
 #PDS init
 pds_tpc0 = l.get_pmt_list(tpc=0,coatings=COATINGS) #Get list of pmts
 pds_tpc1 = l.get_pmt_list(tpc=1,coatings=COATINGS) #Get list of pmts
-cmax = np.max([pds.op_pe.op_pe.sum() for pds in pds_tpc0+pds_tpc1])
+if CMAX == 'global':
+    cmax = np.max([pds.op_pe.op_pe.sum() for pds in pds_tpc0+pds_tpc1])
+elif CMAX == 'dynamic':
+    cmax = np.max([pds.get_pe_start_stop(start_bin,end_bin) for pds in pds_tpc0+pds_tpc1])
+else:
+    if VERBOSE: print(f'{CMAX} is not a valid setting for setting pe color')
+    cmax = None
 cmin = 0
 pds_coordinates_tpc0 = [pds.plot_coordinates(start_bin,end_bin,cmin=cmin,cmax=cmax) for pds in pds_tpc0]
 pds_coordinates_tpc1 = [pds.plot_coordinates(start_bin,end_bin,cmin=cmin,cmax=cmax) for pds in pds_tpc1]
@@ -134,7 +140,8 @@ app.layout = html.Div(style={'display':'flex'},children=[
                         height=800,
                         showlegend=False,
                         xaxis=dict(range=[0, 500]),  # Set x-axis limits
-                        yaxis=dict(range=[-200, 200])   # Set y-axis limits
+                        yaxis=dict(range=[-200, 200]),   # Set y-axis limits
+                        coloraxis=dict(cmax=cmax)
                     ))
             ),
             dcc.Graph(
@@ -154,7 +161,8 @@ app.layout = html.Div(style={'display':'flex'},children=[
                         height=800,
                         showlegend=False,
                         xaxis=dict(range=[0, 500]),  # Set x-axis limits
-                        yaxis=dict(range=[-200, 200])   # Set y-axis limits
+                        yaxis=dict(range=[-200, 200]),   # Set y-axis limits
+                        coloraxis=dict(cmax=cmax)
                     ))
             ),
             dcc.Graph(
@@ -216,6 +224,11 @@ def update_tpcs(start_time_bin, end_time_bin, n_clicks,run, subrun, event):
             else:
                 muon_lines_tpc0 = [go.Scatter()]
                 muon_lines_tpc1 = [go.Scatter()]
+            if l.load_mcpart:
+                mcparts = l.get_mcpart_list()
+                mcparts_lines = [mcparts[i].plot_line(i,max_color=len(mcparts)) for i in range(len(mcparts))]
+            else:
+                mcparts_lines = [go.Scatter()]
         else:
             print(f'Run {run} Subrun {subrun} Event {event} not in file')
             if VERBOSE: 
@@ -226,7 +239,13 @@ def update_tpcs(start_time_bin, end_time_bin, n_clicks,run, subrun, event):
     #PDS
     #if VERBOSE: print('Update time window')
     s0 = time()
-    cmax = np.max([pds.op_pe.op_pe.sum() for pds in pds_tpc0+pds_tpc1])
+    if CMAX == 'global':
+        cmax = np.max([pds.op_pe.op_pe.sum() for pds in pds_tpc0+pds_tpc1])
+    elif CMAX == 'dynamic':
+        cmax = np.max([pds.get_pe_start_stop(start_time_bin,end_time_bin) for pds in pds_tpc0+pds_tpc1])
+    else:
+        if VERBOSE: print(f'{CMAX} is not a valid setting for setting pe color')
+        cmax = None
     cmin = 0
     pds_coordinates_tpc0 = [pds.plot_coordinates(start_time_bin,end_time_bin,cmin=cmin,cmax=cmax) for pds in pds_tpc0]
     pds_coordinates_tpc1 = [pds.plot_coordinates(start_time_bin,end_time_bin,cmin=cmin,cmax=cmax) for pds in pds_tpc1]
@@ -244,15 +263,23 @@ def update_tpcs(start_time_bin, end_time_bin, n_clicks,run, subrun, event):
         muon_lines_tpc1 = [go.Scatter()]
     s1 = time()
     if VERBOSE: print(f'Get new muon trajectories : {s1-s0:.2f} s')
+    if l.load_mcpart:
+        mcparts = l.get_mcpart_list()
+        mcparts_lines = [mcparts[i].plot_line(i,max_color=len(mcparts)) for i in range(len(mcparts))]
+    else:
+        mcparts_lines = [go.Scatter()]
+    s2 = time()
+    if VERBOSE: print(f'Get new mcpart trajectories : {s2-s1:.2f} s')
 
     return [go.Figure(data=pds_coordinates_tpc0+muon_lines_tpc0+mcparts_lines,
-                     layout=go.Layout(
+                      layout=go.Layout(
                          autosize=False,
                          width=1000,
                          height=800,
                          showlegend=False,
                          xaxis=dict(range=[0, 500]),  # Set x-axis limits
-                         yaxis=dict(range=[-200, 200])   # Set y-axis limits
+                         yaxis=dict(range=[-200, 200]),   # Set y-axis limits
+                         coloraxis=dict(cmax=cmax)
                      )),
             go.Figure(data=pds_coordinates_tpc1+muon_lines_tpc1+mcparts_lines,
                      layout=go.Layout(
@@ -261,7 +288,8 @@ def update_tpcs(start_time_bin, end_time_bin, n_clicks,run, subrun, event):
                          height=800,
                          showlegend=False,
                          xaxis=dict(range=[0, 500]),  # Set x-axis limits
-                         yaxis=dict(range=[-200, 200])   # Set y-axis limits
+                         yaxis=dict(range=[-200, 200]),   # Set y-axis limits
+                         coloraxis=dict(cmax=cmax)
                      ))]
 
 
@@ -331,4 +359,4 @@ def update_waveform_graph(click_data):
 
 
 if __name__ == '__main__':
-    app.run_server(debug=True)
+    app.run_server(debug=False)